0e81b7b393eeb300fcdba2234128c98c47fcc990
FAIZAN AHMAD <faizanahmad8292@gmail.com>
2023-06-17 14:48:09 +0500

    getter-setters-constructor

--- app/src/main/java/com/example/madrassaapp/Student.java
+++ app/src/main/java/com/example/madrassaapp/Student.java
@@ -1,7 +1,30 @@
 package com.example.madrassaapp;
 
+
 public class Student {
     private String name;
     private int age;
-    private String Class;
+    private String classroom;
+
+    public Student(String name, int age, String aClass) {
+        this.name = name;
+        this.age = age;
+        classroom = aClass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
 }
